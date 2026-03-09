e5528e917478a141c21fe9d0ca381712f8aa5ecb
francis85a <franperezcid85@gmail.com>
2026-03-09 13:31:41 +0100

    fix(test): arreglar bug en los test goles añadidos al diccionario

--- test/test_equipo.py
+++ test/test_equipo.py
@@ -7,19 +7,19 @@ from src.posicion import Posicion
 
 def test_Equipo_listar_jugadores():
     ciudad = Equipo("Canada")
-    ciudad.añadir_jugador(Jugadora(29, "Marie-Philip Poulin",  Posicion.CENTER, 1))
-    ciudad.añadir_jugador(Jugadora(19, "Brianne Jenner",       Posicion.CENTER, 2))
-    ciudad.añadir_jugador(Jugadora(10, "Sarah Fillier",        Posicion.CENTER, 3))
-    ciudad.añadir_jugador(Jugadora(24, "Natalie Spooner",      Posicion.LEFTWING, 4))
-    ciudad.añadir_jugador(Jugadora( 3, "Jocelyne Larocque",    Posicion.LEFT_DEFENSEMAN, 5))
-    ciudad.añadir_jugador(Jugadora(35, "Ann-Renée Desbiens",   Posicion.GOALIE, 6))
+    ciudad.añadir_jugador(Jugadora(29, "Marie-Philip Poulin",  Posicion.CENTER))
+    ciudad.añadir_jugador(Jugadora(19, "Brianne Jenner",       Posicion.CENTER))
+    ciudad.añadir_jugador(Jugadora(10, "Sarah Fillier",        Posicion.CENTER))
+    ciudad.añadir_jugador(Jugadora(24, "Natalie Spooner",      Posicion.LEFTWING))
+    ciudad.añadir_jugador(Jugadora( 3, "Jocelyne Larocque",    Posicion.LEFT_DEFENSEMAN))
+    ciudad.añadir_jugador(Jugadora(35, "Ann-Renée Desbiens",   Posicion.GOALIE))
     print ("test comprobado,")
     print(str(ciudad.listar_jugadores()))
 
 def test_registrar_gol():
 
     ciudad = Equipo("Canada")
-    ciudad.añadir_jugador(Jugadora(29, "Marie-Philip Poulin",  Posicion.CENTER, 1))
+    ciudad.añadir_jugador(Jugadora(29, "Marie-Philip Poulin",  Posicion.CENTER))
     goleadora = ciudad.obtener_jugador(29)
     goleadora.registrar_gol()
     assert goleadora._goles == 1
